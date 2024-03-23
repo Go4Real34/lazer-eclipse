131169692a20989f2c0a02dd8055ae80a909fa0e
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-23 22:57:38 +0300

    initialize Unreal Engine project

---
+++ LazerEclipse/Source/LazerEclipse/LazerEclipse.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LazerEclipse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LazerEclipse, "LazerEclipse" );
