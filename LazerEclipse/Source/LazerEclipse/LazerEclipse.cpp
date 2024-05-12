bf75ff336cb814826bad0996f547b57ea231af29
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-05-12 04:32:14 +0300

    reinitialize Unreal Engine project with FirstPerson pack

--- LazerEclipse/Source/LazerEclipse/LazerEclipse.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LazerEclipse.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LazerEclipse, "LazerEclipse" );
